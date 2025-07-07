8031ec2a314b5fb3a04d0149331b691a02293303
Doit <77947280+kimduhee7524@users.noreply.github.com>
2025-07-07 21:31:45 +0900

    Revert "refactor: React Query로 변경"

--- src/queryKeys/caskKeys.ts
+++
@@ -1,6 +0,0 @@
-export const caskKeys = {
-  all: ['casks'] as const,
-  lists: () => [...caskKeys.all, 'list'] as const,
-  list: (filters: unknown) => [...caskKeys.lists(), { filters }] as const,
-  detail: (id: number) => [...caskKeys.all, 'detail', id] as const,
-};
